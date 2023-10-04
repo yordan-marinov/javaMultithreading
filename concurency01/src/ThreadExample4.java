b967ab5a0748f2f24a49cf648a838043dc03a162
yordanm <y.marinov@yahoo.co.uk>
2023-10-04 20:43:56 +0300

    Adding the example4 of the creating and starting a threads

--- concurency01/src/ThreadExample4.java
+++ concurency01/src/ThreadExample4.java
@@ -1,10 +1,24 @@
 public class ThreadExample4 {
 
     public static void main(String[] args) {
+
+        Runnable runnable = () -> {
+
+            String name = Thread.currentThread().getName();
+            System.out.println("Hello from the thread with the name: " + name);
+        };
+
         Thread thread = new Thread(
-                () -> System.out.println("Hello from the my lambda runnable in this new thread.")
+                () -> {
+                    Thread.currentThread().setName("My Tread with new name");
+                    String name = Thread.currentThread().getName();
+                    System.out.println("Hello from the my lambda runnable in this new thread with the new name: " + name);
+                }
         );
 
         thread.start();
+
+        Thread thread1 = new Thread(runnable);
+        thread1.start();
     }
 }
